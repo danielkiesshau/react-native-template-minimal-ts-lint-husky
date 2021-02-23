52bfed33a7743807d47d8518c8bb01361b5a71fa
Daniel Kiesshau <danielkiesshau@outlook.com>
2021-02-23 17:32:59 -0300

    template files

---
+++ template/babel.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'babel-plugin-root-import',
+      {
+        paths: [
+          {
+            rootPathSuffix: './src/data',
+            rootPathPrefix: 'data',
+          },
+          {
+            rootPathSuffix: './src/domain',
+            rootPathPrefix: 'domain',
+          },
+          {
+            rootPathSuffix: './src/infra',
+            rootPathPrefix: 'infra',
+          },
+          {
+            rootPathSuffix: './src/presentation',
+            rootPathPrefix: 'presentation',
+          },
+          {
+            rootPathSuffix: './src/utils',
+            rootPathPrefix: 'utils',
+          },
+          {
+            rootPathSuffix: './src',
+            rootPathPrefix: '~/',
+          },
+        ],
+      },
+    ],
+    'module:react-native-dotenv',
+  ],
+}
---
+++ template/src/__tests__/unit/index.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import App from 'presentation/App'
+
+test('(Unit) Renders Step One', () => {
+  const { getByText } = render(<App />)
+
+  const FirstStep = getByText('Step One')
+
+  expect(FirstStep).toBeDefined()
+})
